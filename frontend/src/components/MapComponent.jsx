9c315af18a946c077964e2a28742cf7d918b9af8
Aliussman <Aliussman>
2026-09-19 21:02:04 +0530

    fix: replace watermarked CartoDB tiles with clean Esri Dark Gray Canvas tiles

--- frontend/src/components/MapComponent.jsx
+++ frontend/src/components/MapComponent.jsx
@@ -52,10 +52,11 @@ export default function MapComponent({
       >
         <MapUpdater centre={centre} />
 
-        {/* Dark Matter Base Tiles */}
+        {/* High-Performance Dark Slate Base Tiles (No API key / No watermark) */}
         <TileLayer
-          attribution='&copy; <a href="https://carto.com/">CARTO</a>'
-          url="https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png"
+          attribution='&copy; <a href="https://www.esri.com/">Esri</a> &mdash; Esri, DeLorme, NAVTEQ'
+          url="https://server.arcgisonline.com/ArcGIS/rest/services/Canvas/World_Dark_Gray_Base/MapServer/tile/{z}/{y}/{x}"
+          maxZoom={16}
         />
 
         {/* Riskiest Corridors */}
